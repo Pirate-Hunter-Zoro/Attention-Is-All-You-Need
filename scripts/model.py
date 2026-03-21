5b5fe605c1e105990a16d0726b8481a9686bb941
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-03-20 20:32:59 -0500

    model implementation complete

--- scripts/model.py
+++ scripts/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from scripts.config import BATCH_SIZE, N_EMBD, N_HEAD, BLOCK_SIZE, DROPOUT
+from scripts.config import VOCAB_SIZE, N_LAYER, BATCH_SIZE, N_EMBD, N_HEAD, BLOCK_SIZE, DROPOUT
 
 
 class MultiHeadAttention(nn.Module):
@@ -62,3 +62,105 @@ output projection:                              (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
         
         # Throw through a last linear layer
         return self.dropout(self.output_projection(masked_weights))
+    
+
+class FeedForward(nn.Module):
+    # Follows attention layer in the transformer block
+    # "Two linear transformations  with a ReLU in between"
+    
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(N_EMBD, 4*N_EMBD),
+            nn.ReLU(),
+            nn.Linear(4*N_EMBD, N_EMBD),
+            nn.Dropout(DROPOUT),
+        )
+        
+    def forward(self, x):
+        """
+        (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
+        ->
+        (BATCH_SIZE, BLOCK_SIZE, 4*N_EMBD)
+        ->
+        (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
+        """
+        return self.net(x)
+    
+
+class Block(nn.Module):
+    # Single transformer decoder block
+    
+    def __init__(self):
+        super().__init__()
+        self.first_layer_norm = nn.LayerNorm(N_EMBD)
+        self.attention_layer = MultiHeadAttention()
+        self.second_layer_norm = nn.LayerNorm(N_EMBD)
+        self.feed_forward = FeedForward()
+        
+    def forward(self, x):
+        x = x + self.attention_layer(self.first_layer_norm(x))
+        x = x + self.feed_forward(self.second_layer_norm(x))
+        return x
+    
+    
+class MiniGPT(nn.Module):
+    # Wraps all base models together
+    
+    def __init__(self):
+        super().__init__()
+        # For each (indexed) word in our vocabulary, map it to a vector
+        self.global_embedding_table = nn.Embedding(VOCAB_SIZE, N_EMBD)
+        # For each (indexed) word in our context, map it to a vector
+        self.context_embedding_table = nn.Embedding(BLOCK_SIZE, N_EMBD)
+        # Block layers - how many attention heads we have
+        self.block_layers = nn.Sequential(*[Block() for _ in range(N_LAYER)])
+        # Final layer norm
+        self.layer_norm = nn.LayerNorm(N_EMBD)
+        # Once we have a final embedded output, we need to turn that into logits (probabilities-esque) over our vocabulary
+        self.language_head = nn.Linear(N_EMBD, VOCAB_SIZE)
+        
+    def forward(self, x):
+        """Receives vector of token indices
+        
+        Input Embeddigns:
+        x: (BATCH_SIZE, BLOCK_SIZE)
+        -> Embedding table
+        x: (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
+
+        Positional Embeddings:
+        indices: (BLOCK_SIZE)
+        -> Positional Embedding Table
+        indices: (BLOCK_SIZE, N_EMBD)
+        """
+        embeddings = self.global_embedding_table(x)
+        
+        token_indices = torch.arange(BLOCK_SIZE, device=x.device)
+        position_embeddings = self.context_embedding_table(token_indices)
+        
+        # Broadcasting will handle the batch dimension present in the global embeddings
+        positional_embeddings = embeddings + position_embeddings # (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
+        
+        # Now all of our input tokens have been embedded AND this embedding takes position in context into account
+        output_embeddings = self.block_layers(positional_embeddings)
+        output_embeddings = self.layer_norm(output_embeddings) # (BATCH_SIZE, BLOCK_SIZE, N_EMBD)
+        return self.language_head(output_embeddings) # (BATCH_SIZE, BLOCK_SIZE, VOCAB_SIZE)
+    
+    def generate(self, x, max_new_tokens):
+        """Generate new tokens from the input tokens
+
+        Input tokens:
+        x: (BATCH_SIZE, T) -> T is starting sequence length - as we produce new tokens we will only be backing up to BLOCK_SIZE for full context (no farther) to produce the next tokens
+        
+        """
+        # Note we must generate new tokens sequentially because each new token becomes part of the context to generate whatever token follows
+        for _ in range(max_new_tokens):
+            x = x[:, -BLOCK_SIZE:] # Slice to context size
+            # Find logits for next token - only at the last time step
+            all_token_logits = self.forward(x)
+            next_token_logits = all_token_logits[:, -1, :] # All batches, only last position in context, all token probabilities
+            next_token_probs = F.softmax(next_token_logits, dim=-1)
+            # For each batch element, sample next tokens according to the element's token probability distribution
+            next_tokens = torch.multinomial(next_token_probs, num_samples=1) # (BATCH_SIZE, 1) - next token index for each batch
+            x = torch.cat((x, next_tokens), dim=1) # dim 0 is batch, dim 1 is current token indices
+        return x # All of the generated tokens - (BATCH_SIZE, T + max_new_tokens)
